85123b97aa56880c85bae33289e23a7964e90e45
Dave Sag <davesag@gmail.com>
2017-02-05 17:12:18 +0530

    added dummy test as a placeholder

---
+++ test/dummy_spec.js
@@ -0,0 +1,10 @@
+import {expect}   from 'chai'
+import sinon      from 'sinon'
+import proxyquire from 'proxyquire'
+import faker      from 'faker'
+
+describe('dummy', () => {
+  it('does nothing', () => {
+    return expect(true).to.be.true
+  })
+})
